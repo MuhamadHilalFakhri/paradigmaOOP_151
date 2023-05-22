d8a694f99aef8d17ef7c2a417c9269b44a9d6dd1
hilal555 <muhamadhilal04@gmail.com>
2023-05-22 15:25:00 +0700

    menampilkan output ke layar

--- memberintilist/memberintilist.cpp
+++ memberintilist/memberintilist.cpp
@@ -9,4 +9,12 @@ private:
 	float nilai;
 
 public:	
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+
+	}
+	~mahasiswa() {
+		cout << "Id		= " << id << endl;
+		cout << "Nama	= " << id << endl;
+		cout << "Nilai	= " << nilai << endl;
+	}
 };
